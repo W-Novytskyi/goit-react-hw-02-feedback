d70d84de06f842f698bfbcf77e34352016c8fd13
W-Novytskyi <novickijvaceslav011@gmail.com>
2023-04-17 09:15:59 +0300

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 
 class Counter extends React.Component {
+  static propTypes = {
+    //
+  };
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
+  handleIncrementGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleIncrementNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleIncrementBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
   render() {
     return (
       <div>
-        <button type="button">Good</button>
-        <button type="button">Neutral</button>
-        <button type="button">Bad</button>
+        <button type="button" onClick={this.handleIncrementGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.handleIncrementNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.handleIncrementBad}>
+          Bad
+        </button>
         <p>Statistics</p>
         <span>Good:{this.state.good}</span>
         <span>Neutral:{this.state.neutral}</span>
